11a016e5e56bc814220d72ae0d2277a05a383a7a
Fleckism <Jamie.A.Fleck@gmail.com>
2021-06-22 16:55:07 -0400

    Removing dual boot

--- Functions.cpp
+++ Functions.cpp
@@ -11,7 +11,6 @@ T operator-(T x);           // -a calls function operator-(a)
 T operator++(int);          // postfix ++ or -- (parameter ignored)
 extern "C" {void f();}      // f() was compiled in C
 
-// Function parameters and return values may be of any type. A function must either be declared or defined before
 // it is used. It may be declared first and defined later. Every program consists of a set of a set of global variable
 // declarations and a set of function definitions (possibly in separate files), one of which must be:
 
@@ -25,7 +24,7 @@ int main(int argc, char* argv[]) { statements... }
 //
 // Functions with different parameters may have the same name (overloading). Operators except `::` `.` `.*` `?:` may be overloaded.
 // Precedence order is not affected. New operators may not be created.
-
+// ************************************************************************
 Concept of Function:
 	Is a mapping from a set A (the domain) to a set B (the range) such that with every element in A, the function associates exactly one element in B.
 
